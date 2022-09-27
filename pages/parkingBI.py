f1929ab473c51cc3b7439c946e9e81ef26bf92e3
ColdTonic <tommy.sam31@gmail.com>
2022-09-27 21:12:15 +1000

    t

--- pages/parkingBI.py
+++ pages/parkingBI.py
@@ -6,5 +6,4 @@ st.subheader('hello')
 st.components.v1.html("""<iframe title="parkingbi" src="https://app.powerbi.com/reportEmbed?reportId=cb31e468-2410-4b5b-825c-cef573f14a49&autoAuth=true&ctid=df7f7579-3e9c-4a7e-b844-420280f53859" frameborder="0" allowFullScreen="true"></iframe>""", width=1140, height=600
 
 
-"https://app.powerbi.com/reportEmbed?reportId=cb31e468-2410-4b5b-825c-cef573f14a49&autoAuth=true&ctid=df7f7579-3e9c-4a7e-b844-420280f53859", unsafe_allow_html=True)
 #https://app.powerbi.com/links/GQQYSYJxUj?ctid=df7f7579-3e9c-4a7e-b844-420280f53859&pbi_source=linkShare
