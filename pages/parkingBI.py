27a47042f6f21655146c0782c6c1ce7f29e553aa
ColdTonic <tommy.sam31@gmail.com>
2022-09-27 20:56:47 +1000

    t

--- pages/parkingBI.py
+++ pages/parkingBI.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.header('Welcome to the Parking Data Streamlit Web App')
 st.subheader('This section is displays general information regarding parking data across melbourne run via PowerBI. See the other pages for more information.')
 st.subheader('hello')
-st.components.v1.html('https://app.powerbi.com/reportEmbed?reportId=cb31e468-2410-4b5b-825c-cef573f14a49&autoAuth=true&ctid=df7f7579-3e9c-4a7e-b844-420280f53859')
+st.markdown("https://app.powerbi.com/reportEmbed?reportId=cb31e468-2410-4b5b-825c-cef573f14a49&autoAuth=true&ctid=df7f7579-3e9c-4a7e-b844-420280f53859", unsafe_allow_html=True)
 #https://app.powerbi.com/links/GQQYSYJxUj?ctid=df7f7579-3e9c-4a7e-b844-420280f53859&pbi_source=linkShare
